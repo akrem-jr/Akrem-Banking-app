e733475acaf333d4fa75c691abb43a13eed608ae
akrem0-jr <abdulbasetadem@gmail.com>
2024-10-25 15:36:31 +0300

    update home page header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,20 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { FirstName :'akrem '};
   return (
    <section className='home'>
 
     <div className='home-content'>
         <header className='home-header'>
-            Welcome , Akrem
+            <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.FirstName || "akrem jr "}
+            subtext="Access and manage your account and transactions efficiently."
+            
+            />
         </header>
     </div>
    </section>
